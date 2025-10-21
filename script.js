855bcf9d4945cbd81fdcfa3b7a4a1858e4a95fa6
Dudu <eduardo.machry@escola.pr.gov.br>
2025-10-21 10:30:18 -0300

    21/10/2025

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener("click",mostraPalavrasChave);
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave();{ 
-  const texto = document.querySelector("#entrada-de-texto").value
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-  const palavras = texto.split("  ");
+function mostraPalavrasChave(){
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavras = texto.split(" ");
 
-   campoResultado.textContent = palavras.join(", ")
+    campoResultado.textContent = palavras.join(", ")
 }
-   
+
+
