aed8f7b6264b09d9a6fba4f15e9b9b6de690dd8b
Dudu <eduardo.machry@escola.pr.gov.br>
2025-10-21 10:12:53 -0300

    21/10/2025

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function mostraPalavrasChave();{
   const texto = document.querySelector("#entrada-de-texto").value
   const campoResultado = document.querySelector("#resultado-palavrachave");
   const palavras = texto.split("  ");
-  alert("eu - Eduardo - dou a bunda para o pt")
+
+   campoResultado.textContent = palavras.join(", ")
 }
    
