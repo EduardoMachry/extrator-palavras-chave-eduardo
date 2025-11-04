3de99d05981b75f35e8a0c0210109e3ab678a7a4
Dudu <eduardo.machry@escola.pr.gov.br>
2025-11-04 09:25:56 -0300

    04/11/2025

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const botcaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
@@ -40,5 +40,5 @@ function contaFrequencias(palavras) {
 
 
 
-    
+
 }
