bf44a83a5490a0a03014b5694a411043ed4cafac
Dudu <eduardo.machry@escola.pr.gov.br>
2025-11-11 09:27:15 -0300

    11/11/2025

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
@@ -45,7 +46,6 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que"]);
     const palavrasBoas = [];
         for (let palavra of palavras) {
         if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
